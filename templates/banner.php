9cdb664d1ec813a9ea9d1cd2841d73418471cc48
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-02-18 14:07:06 +0100

    Dans banner.php on change le titre du site par la fonction get_bloginfo('name') afin de récupérer dynamiquement le titre du site que l'on peut changer dans la partie personnalisation du thème puis identité du thème.

--- templates/banner.php
+++ templates/banner.php
@@ -2,7 +2,8 @@
   <header class="masthead bg-primary text-white text-center">
     <div class="container">
       <img class="img-fluid mb-5 d-block mx-auto" src="http://localhost:8080/wp-content/themes/coding-theme/img/profile.png" alt="">
-      <h1 class="text-uppercase mb-0">Start Bootstrap</h1>
+      <!-- https://developer.wordpress.org/reference/functions/get_bloginfo/ -->
+      <h1 class="text-uppercase mb-0"><?php echo get_bloginfo('name'); ?></h1>
       <hr class="star-light">
       <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
     </div>
