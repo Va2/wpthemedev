c8b03b566fb52d77bc189707bf90d5e615754ca8
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-02-18 16:00:40 +0100

    Ajout d'un filtre pour pouvoir ajouter l'attribut class pour chaque balise <a>. Suppression de l'ancien menu dans le header

--- functions.php
+++ functions.php
@@ -46,3 +46,22 @@ function register_main_menu()
 
 
 add_action('after_setup_theme', 'register_main_menu');
+
+/**
+ * Fonction qui ajoute des attributes au balise a des nav_menu
+ *
+ * @param [type] $att
+ * @param [type] $item
+ * @param [type] $args
+ * @return void
+ */
+function ajout_menu_a_class($atts, $item, $args)
+{
+  $class = 'nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger'; // or something based on $item
+  $atts['class'] = $class;
+  return $atts;
+}
+
+// Ajout d'un écouteur d'événement de type filtre qui nous permet de changer les attributs des balises <a>
+// les add_action et add_filter peuvent avoit jusqu'à 4 paramêtre. Le 3ème pour l'ordre d'execution et le 4 ème pour le nombre de parammètre qui seront passer à la fonction callback
+add_filter('nav_menu_link_attributes', 'ajout_menu_a_class', 10, 3);
