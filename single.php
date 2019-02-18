5754cc6bc9285ae88b7b2b4097b94d268b3e5fcf
Yassine Kharchaf <yassine.kharchaf@gmail.com>
2019-02-18 16:44:55 +0100

    Nous avons créé un article avec un titre et un contenu. lorsque nous visionons cette article nous tombons sur la page index.php. nous avons donc créé un fichier single.php qui reprend le header.php et le footer. entre les deux, une div avec une class single-post-container pour pouvoir appliquer une css dans la personnalisation avec un margin-top: 106px;. A l'intérieur de la div, un h2 avec pour texte le titre de l'article que l'on affiche grâce à la fonction the_title(). L'article à également une catégorie logo

---
+++ single.php
@@ -0,0 +1,14 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="container single-post-container">
+  <h2 class="text-center text-uppercase text-secondary mb-0"><?php the_title(); ?></h2>
+</div>
+<?php
+
+get_footer();
+
+?>
